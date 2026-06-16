5c2f96cb582926bc3edb3f217d8884d79f50bff8
조남윤 <southdbswh@soongsil.ac.kr>
2026-06-16 20:47:11 +0900

    초기화

--- C++공부/C++공부.cpp
+++ C++공부/C++공부.cpp
@@ -1,13 +1,6 @@
 ﻿#include <iostream>
 using namespace std;
 int main()
-{	
-	int i, j;
-	cout << "Enter num_1 : ";
-	cin >> i;
-	cout << "Enter num_2 : ";
-	cin >> j;
-
-	cout << "Sum : " << i + j << endl;
+{
 	return 0;
 }
